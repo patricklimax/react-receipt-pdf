ec7037712c4763acb4bb1c4f2c94cc32d9924283
patricklimax <patricklimax@outlook.com.br>
2024-05-30 00:36:32 -0300

    using shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,35 @@
 'use client'
 import { useState } from 'react';
+import generatePDF, { Resolution, Margin, Options } from "react-to-pdf";
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
+import { Button } from '@/components/ui/button';
+import { Card } from '@/components/ui/card';
+
+const options: Options = {
+  method: "open",
+  resolution: Resolution.HIGH,
+  page: {
+    margin: Margin.MEDIUM,
+    format: "letter",
+    orientation: "portrait",
+  },
+};
+
+const numero = require('numero-por-extenso');
+
+const date = new Date().toLocaleDateString('pt-BR', {
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+})
+
+
 
 export default function Home() {
+
   const [benefiario, setBenefiario] = useState('')
   const [cpfBeneficiario, setCpfBeneficiario] = useState('')
   const [stateUF, setStateUF] = useState('')
@@ -9,143 +37,173 @@ export default function Home() {
   const [pagador, setPagador] = useState('')
   const [cpfPagador, setCpfPagador] = useState('')
   const [telPagador, setTelPagador] = useState('')
+  const [receiptValue, setReceiptValue] = useState('')
   const [description, setDescription] = useState('')
   const [observation, setObservation] = useState('')
 
+  const openPDF = () => {
+    generatePDF(() => document.getElementById("wrapper"), options);
+  };
+
 
   return (
-    <main className="flex min-h-screen flex-col gap-2 p-2 py-6 bg-zinc-100">
-      <h1 className='w-full flex justify-center items-center font-bold text-2xl'>
-        EMISSOR DE RECIBO EM PDF
+    <main className="flex flex-col items-center gap-2 p-2 py-6">
+      <h1 className='w-full text-center font-bold text-2xl p-4'>
+        EMISSOR DE RECIBO
       </h1>
+      <Tabs defaultValue="receiptData" className="w-3/5">
+        <TabsList className='flex'>
+          <TabsTrigger className='flex-1' value="receiptData">
+            DADOS DO RECIBO
+          </TabsTrigger>
+          <TabsTrigger className='flex-1' value="receiptView">
+            VISUALIZAR
+          </TabsTrigger>
+        </TabsList>
 
-      <div className='w-full flex h-full gap-10 px-10'>
-        <div className='w-1/2 flex flex-col p-4 border rounded'>
-          <h1 className='text-center font-semibold text-3xl uppercase bg-zinc-900 text-zinc-200'>
-            Dados do Recibo
-          </h1>
-          <div className='flex flex-col gap-2 py-2'>
-            <div >
-              <h1 className='font-semibold ml-1'>
-                Dados do Beneficiário:
+        <TabsContent value="receiptData" >
+          <Card className='flex flex-col gap-5 p-4'>
+            <div className='flex gap-5'>
+              <Card id='pagador' className='p-4'>
+                <h1 className='font-semibold ml-1 mb-1'>Dados do Pagador</h1>
+                <div className='flex flex-col gap-2'>
+                  <Input
+                    type="text"
+                    value={pagador}
+                    onChange={e => setPagador(e.target.value)}
+                    className='rounded p-2 text-sm w-full'
+                    placeholder='Nome do Pagador' />
+
+                  <div className='flex gap-2'>
+                    <Input
+                      type="number"
+                      value={cpfPagador}
+                      onChange={e => setCpfPagador(e.target.value)}
+                      className='rounded p-2 text-sm w-1/2'
+                      placeholder='CPF' />
+                    <Input
+                      type="number"
+                      value={telPagador}
+                      onChange={e => setTelPagador(e.target.value)}
+                      className='rounded p-2 text-sm w-1/2'
+                      placeholder='Telefone' />
+                  </div>
+                </div>
+              </Card>
+
+              <Card className='p-4' id='beneficiario'>
+                <h1 className='font-semibold ml-1 mb-1'>
+                  Dados do Beneficiário
+                </h1>
+                <div className='flex flex-col gap-2'>
+                  <Input
+                    type="text"
+                    value={benefiario}
+                    onChange={e => setBenefiario(e.target.value)}
+                    className='rounded p-2 text-sm w-full'
+                    placeholder='Nome do Beneficiário' />
+
+                  <div className='flex gap-2 '>
+                    <Input
+                      type="number"
+                      value={cpfBeneficiario}
+                      onChange={e => setCpfBeneficiario(e.target.value)}
+                      className='rounded p-2 text-sm w-1/3'
+                      placeholder='CPF' />
+
+                    <Input
+                      type="text"
+                      value={stateUF}
+                      onChange={e => setStateUF(e.target.value)}
+                      className='rounded p-2 text-sm w-1/3'
+                      placeholder='Estado' />
+                    <Input
+                      type="text"
+                      value={city}
+                      onChange={e => setCity(e.target.value)}
+                      className='rounded p-2 text-sm w-1/3'
+                      placeholder='Município' />
+                  </div>
+                </div>
+              </Card>
+            </div>
+
+
+            <Card id='values' className='p-4'>
+              <h1 className='font-semibold ml-1 mb-1'>Valor do Recibo</h1>
+              <Input
+                type="number"
+                value={receiptValue}
+                onChange={e => setReceiptValue(e.target.value)}
+                className='rounded p-2 text-sm w-1/2'
+                placeholder='Valor do Recibo' />
+            </Card>
+
+            <div id='details' className='flex  gap-5'>
+              <Card id='description' className='flex-1 p-4'>
+                <h1 className='font-semibold ml-1 mb-1'>Descrição do Serviço/Produto</h1>
+                <Textarea
+                  className='w-full rounded p-2 text-sm'
+                  placeholder='Descreva aqui'
+                  value={description}
+                  onChange={e => setDescription(e.target.value)} />
+              </Card>
+
+              <Card id='observation' className='flex-1 p-4'>
+                <h1 className='font-semibold ml-1 mb-1'>Observação:</h1>
+                <Textarea
+                  className='w-full rounded p-2 text-sm'
+                  placeholder='Descreva aqui'
+                  value={observation}
+                  onChange={e => setObservation(e.target.value)} />
+              </Card>
+            </div>
+          </Card>
+        </TabsContent>
+
+        <TabsContent value="receiptView">
+          <Card className='flex flex-col gap-5 p-4'>
+            <div className='p-20' id="wrapper">
+
+              <h1 className='text-4xl text-center font-semibold'>
+                Recibo de Pagamento
               </h1>
-              <div className='flex flex-col gap-2'>
-                <input
-                  type="text"
-                  value={benefiario}
-                  onChange={e => setBenefiario(e.target.value)}
-                  className='rounded p-2 text-sm w-full'
-                  placeholder='Informe o nome do Beneficiário' />
 
-                <div className='flex gap-2 '>
-                  <input
-                    type="number"
-                    value={cpfBeneficiario}
-                    onChange={e => setCpfBeneficiario(e.target.value)}
-                    className='rounded p-2 text-sm w-1/3'
-                    placeholder='CPF do Beneficiário' />
+              <p className='text-right font-semibold text-3xl my-16'>
+                {parseFloat(receiptValue).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}
+              </p>
 
-                  <input
-                    type="text"
-                    value={stateUF}
-                    onChange={e => setStateUF(e.target.value)}
-                    className='rounded p-2 text-sm w-1/3'
-                    placeholder='Estado' />
-                  <input
-                    type="text"
-                    value={city}
-                    onChange={e => setCity(e.target.value)}
-                    className='rounded p-2 text-sm w-1/3'
-                    placeholder='Município' />
+
+              <p className='text-justify mb-20'>
+                Eu, <span className='font-semibold'>{benefiario}</span>, CPF nº <span className='font-semibold'>{cpfBeneficiario}</span>, DECLARO que recebi do Sr.(a) <span className='font-semibold'>{pagador}</span>, CPF nº <span className='font-semibold'>{cpfPagador}</span>, <span>Telefone: {telPagador},</span> a quantia de {parseFloat(receiptValue).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })} ({numero.porExtenso(receiptValue, numero.estilo.monetario)}), referente ao pagamento {description}.
+              </p>
+
+              {
+                observation && <div className='text-justify mb-20'>
+                  <p className='font-semibold'>Observação:</p>
+                  {observation}
                 </div>
+              }
+
+
+              <p className='mb-16 text-center'>
+                {city}, {stateUF}, {date}.
+              </p>
+
+              <div className='text-center'>
+                <p>{benefiario}</p>
+                <p>CPF: {cpfBeneficiario}</p>
               </div>
             </div>
 
-            <div >
-              <h1 className='font-semibold ml-1'>Dados do Pagador</h1>
-              <div className='flex flex-col gap-2'>
-                <input
-                  type="text"
-                  value={pagador}
-                  onChange={e => setPagador(e.target.value)}
-                  className='rounded p-2 text-sm w-full'
-                  placeholder='Nome do Pagador' />
 
-                <div className='flex gap-2'>
-                  <input
-                    type="number"
-                    value={cpfPagador}
-                    onChange={e => setCpfPagador(e.target.value)}
-                    className='rounded p-2 text-sm w-1/2'
-                    placeholder='CPF do Pagador' />
-                  <input
-                    type="number"
-                    value={telPagador}
-                    onChange={e => setTelPagador(e.target.value)}
-                    className='rounded p-2 text-sm w-1/2'
-                    placeholder='Telefone do Pagador' />
-                </div>
-              </div>
+            <div className='flex justify-center items-center p-5 gap-32'>
+              <Button variant="outline">Novo Recibo</Button>
+              <Button onClick={openPDF}>Gerar Recibo</Button>
             </div>
-
-            <div >
-              <h1 className='font-semibold ml-1'>Descrição do Serviço/Produto</h1>
-              <div className='flex flex-col '>
-                <div>
-                  <textarea
-                    className='w-full rounded p-2 text-sm'
-                    placeholder='Descreva aqui'
-                    value={description}
-                    onChange={e => setDescription(e.target.value)}
-                  ></textarea>
-                </div>
-              </div>
-            </div>
-
-            <div >
-              <h1 className='font-semibold ml-1'>Observação</h1>
-              <div className='flex flex-col '>
-                <div>
-                  <textarea
-                    className='w-full rounded p-2 text-sm'
-                    placeholder='Descreva aqui'
-                    value={observation}
-                    onChange={e => setObservation(e.target.value)}
-                  ></textarea>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className='w-1/2 h-full flex flex-col p-4 border rounded bg-zinc-200'>
-          <h1 className='text-center font-semibold text-3xl uppercase bg-zinc-900 text-zinc-200'>
-            Seu recibo ficará assim
-          </h1>
-          <div className='flex flex-col gap-4'>
-            <h1 className='text-3xl text-center font-semibold'>Recibo de Pagamento</h1>
-            <p className='text-right font-semibold text-2xl'>R$ 500,00</p>
-            <p className='text-justify'>
-              Eu, <span className='font-semibold'>{benefiario}</span>, CPF nº {cpfBeneficiario}, DECLARO que recebi do Sr.(a) {pagador}, CPF nº {cpfPagador}, <span>Telefone: {telPagador},</span> a quantia de R$ 500,00 (quinhentos reais), referente ao pagamento {description}.
-            </p>
-
-            <div>
-              <p>Observação:</p>
-              {observation}
-            </div>
-
-            <p className='mt-5 text-center'>{city}, {stateUF}, 25 de maio de 2024.</p>
-            <div className='text-center mt-5'>
-              <p>{benefiario}</p>
-              <p>CPF: {cpfBeneficiario}</p>
-            </div>
-
-          </div>
-          <div className='p-10 flex justify-center items-center'>
-            GERAR RECIBO EM PDF
-          </div>
-        </div>
-      </div>
+          </Card>
+        </TabsContent>
+      </Tabs>
     </main>
   );
 }
